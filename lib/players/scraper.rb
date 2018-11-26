44713e362a022d5bf132c6fa603fbf32b11d70d0
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-26 02:14:07 +0000

    scraper into list of players

--- lib/players/scraper.rb
+++ lib/players/scraper.rb
@@ -1,3 +1,15 @@
 class Scraper
-
+  def get_page
+    doc = Nokogiri::HTML (open("https://www.newyorkredbulls.com/players"))
+  end
+  def get_players
+    self.get_page.css('li.row')
+  end
+  def make_player
+    self.get_players.collect do |players|
+      player = Player.new
+      player.name = players.css('div.player_info div.name a').children.first.text
+      player.position = players.css('div.player_info span.position').children.first.text
+      #binding.pry
+    end
 end
